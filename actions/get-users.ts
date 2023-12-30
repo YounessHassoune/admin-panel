0108281808800df27ada657889d3277f7a4fc593
YounessHassoune <yhassoune7@gmail.com>
2023-12-30 18:13:51 +0100

    fix : filter users based on role

--- actions/get-users.ts
+++ actions/get-users.ts
@@ -9,12 +9,22 @@ export async function getUsers() {
   const {
     data: { users },
   } = await supabase.auth.admin.listUsers();
-
-  const transformedUsers = users.map((user) => ({
-    id: user.id,
-    email: user.email!,
-    role: user.user_metadata?.user_role ?? "SuperAdmin",
-    lastSignIn: user.last_sign_in_at ? new Date(user.last_sign_in_at) : null,
-  }));
+  //get user session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  //check user role to filter users
+  const role = session?.user?.user_metadata?.user_role;
+  // filter user depending on the role
+  const transformedUsers = users
+    .filter((user) =>
+      role === "Admin" ? user.user_metadata?.user_role === "Admin" : true
+    )
+    .map((user) => ({
+      id: user.id,
+      email: user.email!,
+      role: user.user_metadata?.user_role ?? "SuperAdmin",
+      lastSignIn: user.last_sign_in_at ? new Date(user.last_sign_in_at) : null,
+    }));
   return transformedUsers;
 }
