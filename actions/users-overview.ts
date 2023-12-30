f8670e76865d3c669c3d004c3bc53612eabda719
YounessHassoune <yhassoune7@gmail.com>
2023-12-30 18:40:07 +0100

    fix :  users overview import

--- actions/users-overview.ts
+++ actions/users-overview.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { Count } from "@/components/users-overview";
+import { Count } from "@/constants";
 import { createClient } from "@/lib/supabase/server";
 import { cookies } from "next/headers";
 
