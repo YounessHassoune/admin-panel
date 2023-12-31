adfc487cbfe25ae5a37e717775ef803e55ffff01
YounessHassoune <yhassoune7@gmail.com>
2023-12-31 19:06:32 +0100

    feat : resend mail and readme

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# Admin Panel
+# Admin Panel Project
+
+This project is an admin panel built using Next.js and supabase that allows managing admin lists, businesses, and viewing global statistics on the dashboard page.
+
+## Features
+
+- **Admin Management**: Ability to view and manage admin users.
+- **Businesses Management**: Creation and management of businesses.
+- **Dashboard**: View global statistics and insights.
+
+## Installation
+
+### Prerequisites
+
+- `Node.js` (v16 or above)
+- `pnpm` package manager
+
+### Steps
+
+1. Clone the repository:
+
+   ```bash
+   git clone <repository_url>
+   cd admin-panel
+   ```
+
+2. Install dependencies:
+
+   ```bash
+   pnpm install
+   ```
+
+3. Set up environment variables:
+
+   Create a .env.local file in the root of the project and add the following:
+
+   ```dotenv
+   NEXT_PUBLIC_SUPABASE_URL=https://dclvbijgozjuyrwbjcgd.supabase.co
+   NEXT_PUBLIC_SUPABASE_ANON_KEY=<Your Supabase Anon Key>
+   SERVICE_ROLE_KEY=<Your Service Role Key>
+   RESEND_API_KEY=<Your Resend API Key>
+   NEXT_PUBLIC_VERCEL_URL=<Your Vercel URL>
+   ```
+
+4. Run the development server:
+
+   ```bash
+   pnpm dev
+   ```
+
+5. Open http://localhost:3000 to view the project in your browser.
+
+## Live Production Preview
+
+[Live Preview Link](https://admin-panel-ten-gray.vercel.app/login)
+
+## Creator
+
+- **Name:** Youness Hassoune
+- **website:** [younesshassoune](https://younesshassoune.com/)
--- actions/new-user.ts
+++ actions/new-user.ts
@@ -5,21 +5,45 @@ import { createClient } from "@/lib/supabase/server";
 import { cookies } from "next/headers";
 import { generate } from "generate-password";
 import { revalidatePath } from "next/cache";
+import { Resend } from "resend";
 
 export async function createNewUser(data: { email: string; role: RoleType }) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const cookieStore = cookies();
   const supabase = await createClient(cookieStore);
+  //generate password for the user
   const password = generate({
     length: 6,
     numbers: true,
     symbols: true,
   });
+  // create the user
   const result = await supabase.auth.admin.createUser({
     email: data.email,
     password,
     user_metadata: { user_role: data.role },
     email_confirm: true,
   });
+  // send the password via email
+  if (result.data) {
+    try {
+      const result = await resend.emails.send({
+        from: "Admin Panel <admin.panel@younesshassoune.com>",
+        to: [data.email],
+        subject: "Login Password ",
+        html: `
+        <p>Hello,</p>
+        <p>Here are your login details:</p>
+        <p>Email: ${data.email}</p>
+        <p>Password: ${password}</p>
+        <p>Login here: <a href="${process.env.NEXT_PUBLIC_VERCEL_URL}/login">Login Link</a></p>
+        <p>Thank you!</p>
+      `,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   revalidatePath("/users");
   // !!if user is created mail this to the user
   return result;
